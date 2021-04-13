e8192187751c20deea7505dbfb52ebe21125e10c
Farhan <farhanisto@gmail.com>
2021-04-13 14:12:10 +0300

    signout

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Login from './components/Auth/Login'
+import Register from './components/Auth/Register'
+import Signout from './components/Auth/Signout'
+import Layout from './components/hocs/Layout'
+import Home from './components/Home'
 
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from './components/Auth/Login';
-import Register from './components/Auth/Register';
-import Layout from './components/hocs/Layout';
-import Home from './components/Home';
-
-import { useQuery, gql } from '@apollo/client';
-
+import { useQuery, gql } from '@apollo/client'
 
 const App = () => (
-    <Router>
-        <Layout>
-            <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/login' component={Login} />
-                <Route exact path='/register' component={Register}/>
-            </Switch>
-        </Layout>
-    </Router>
-);
+  <Router>
+    <Layout>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/signout" component={Signout} />
+      </Switch>
+    </Layout>
+  </Router>
+)
 
-export default App;
+export default App
